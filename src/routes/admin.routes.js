776465849944a9ceba9ce306fb50a3b8569ad487
Julio González <juliogtrenard@gmail.com>
2025-12-04 16:34:54 +0100

    FIX url de crear peliculas admin

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -2,18 +2,20 @@
 const express = require("express");
 const router = express.Router();
 
-const multer = require('multer');
-const upload = multer({ dest: 'public/uploads' })
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads" });
 
 //importaciones locales
-const {crearPelicula, crearPeliculaForm}=require('../controllers/admin.controllers')
+const {
+    crearPelicula,
+    crearPeliculaForm,
+} = require("../controllers/admin.controllers");
 
 // mostrar formulario
-router.get('/createmovie',crearPeliculaForm)
+router.get("/createmovie", crearPeliculaForm);
 
 //llamada al controlador del back para insertar lo que se ha recibido por formulario
-router.post('/movies/createmovie',upload.single('url_imagen'),crearPelicula)
-
+router.post("/createmovie", upload.single("url_imagen"), crearPelicula);
 
 //exporta el objeto
-module.exports=router
+module.exports = router;
